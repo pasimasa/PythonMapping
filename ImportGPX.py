56f33d3afe1307e79c02d50b6f6c5b6f5318332b
Pasi Hyvonen <phyvonen@outlook.com>
2014-03-14 11:27:46 +0000

    Add importgpx.py script

---
+++ ImportGPX.py
@@ -0,0 +1,17 @@
+import sys, arcpy, os
+
+gpxFolder = r"c:\stuff\python\gpx"
+
+target = r"c:\stuff\python\GPS.gdb\Tracks_1"
+
+arcpy.env.overwriteOutput = True
+
+for f in os.listdir(gpxFolder):
+    print f
+    arcpy.GPXtoFeatures_conversion(gpxFolder + "\\" + f, "in_memory\\track")
+    
+		print "convert to in_mem"
+    arcpy.PointsToLine_management("in_memory\\track", "in_memory\\track_line")
+    
+		print "converted to line"
+    arcpy.Append_management("in_memory\\track_line", target, "NO_TEST")
