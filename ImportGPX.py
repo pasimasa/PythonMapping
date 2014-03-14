54b01d290c7431afdc27ab678215b57a574ebbb5
Pasi Hyvonen <phyvonen@outlook.com>
2014-03-14 11:52:02 +0000

    Update ImportGPX.py

--- ImportGPX.py
+++ ImportGPX.py
@@ -2,7 +2,9 @@
 Script to import all GPX tracks from a folder into a line feature class
 in a file geodatabase. New line feature for each GPX file.
 
-Paths hardcoded in script so needs changed first before running it
+Paths hardcoded in script so needs changed first before running it.
+
+Could do with some error handling, create FGDB if it doesn't exist etc
 """
 
 import sys, arcpy, os
@@ -14,11 +16,12 @@ target = r"c:\stuff\python\GPS.gdb\Tracks_1"
 arcpy.env.overwriteOutput = True
 
 for f in os.listdir(gpxFolder):
-    print f
-    arcpy.GPXtoFeatures_conversion(gpxFolder + "\\" + f, "in_memory\\track")
+	
+	print f
+	arcpy.GPXtoFeatures_conversion(gpxFolder + "\\" + f, "in_memory\\track")
     
-		print "convert to in_mem"
-    arcpy.PointsToLine_management("in_memory\\track", "in_memory\\track_line")
-    
-		print "converted to line"
-    arcpy.Append_management("in_memory\\track_line", target, "NO_TEST")
+	print "convert points to lines in memory"
+	arcpy.PointsToLine_management("in_memory\\track", "in_memory\\track_line")
+    	
+    print "add the new in_mem line to target"
+	arcpy.Append_management("in_memory\\track_line", target, "NO_TEST")
