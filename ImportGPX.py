152e5b5bf4263b60c1b988721a7f65480387547d
Pasi Hyvonen <phyvonen@outlook.com>
2014-03-21 16:53:04 +0000

    add truncate

--- ImportGPX.py
+++ ImportGPX.py
@@ -7,6 +7,7 @@ Generate linear density map of those lines to show where most tracks go.
 Paths hardcoded in script so needs changed first before running it.
 
 Could do with some error handling, create FGDB if it doesn't exist etc
+Also doesn't carry any attributes through from GPX, date/time would be good
 """
 
 import sys, arcpy, os
@@ -15,18 +16,29 @@ gpxFolder = r"c:\stuff\python\gpx"
 
 target = r"c:\stuff\python\GPS.gdb\Tracks_1"
 
-arcpy.env.overwriteOutput = True
+def main():
 
-for f in os.listdir(gpxFolder):
-	
-	print f + " - convert to points in_mem"
-	arcpy.GPXtoFeatures_conversion(gpxFolder + "\\" + f, "in_memory\\track")
-    
-	print "convert in_memory points to lines"
-	arcpy.PointsToLine_management("in_memory\\track", "in_memory\\track_line")
-    	
-    print "add the new in_mem line to target"
-	arcpy.Append_management("in_memory\\track_line", target, "NO_TEST")
-	
-	# Line density stuff todo - needs Spatial Analyst
-	# lineDensity = LineDensity(target, "", 10, 20)
+    print "Truncating %s..." % target
+    try:
+        arcpy.TruncateTable_management(target)
+    except:
+        print "Target not found or can't truncate"
+        sys.exit()
+
+    for f in os.listdir(gpxFolder):
+        print "Processing file %s" % f
+
+        print "\tconvert to points in_mem"
+        arcpy.GPXtoFeatures_conversion(gpxFolder + "\\" + f, "in_memory\\track")
+
+        print "\tconvert in_memory points to lines"
+        arcpy.PointsToLine_management("in_memory\\track", "in_memory\\track_line")
+
+        print "\tadd the new in_mem line to target feature class"
+        arcpy.Append_management("in_memory\\track_line", target, "NO_TEST")
+
+        # Line density stuff todo - needs Spatial Analyst
+        # lineDensity = LineDensity(target, "", 10, 20)
+
+if __name__ == '__main__':
+    main()
