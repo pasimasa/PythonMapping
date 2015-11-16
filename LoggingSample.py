9874b6a2bdfa289e6a533882f94e8d442093ceb4
Pasi Hyvonen <pasimasa@users.noreply.github.com>
2015-11-16 15:48:03 +0000

    add log output to screen

--- LoggingSample.py
+++ LoggingSample.py
@@ -4,16 +4,23 @@ VERSION = "1.0"
 
 def SetupLogger():
     """
-    Create logger object for writing into log file. 
+    Create logger object for writing into log file and output to screen. 
     Creates new log for each day (good for scheduled tasks)
     """
     logger = logging.getLogger(VERSION)
     logger.setLevel(logging.DEBUG)
     now = datetime.datetime.now()
-    handler = logging.FileHandler("ScriptLog_%s_%s_%s.log" % (now.day, now.month, now.year), "a", encoding=None, delay="true")
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    # Output to log file
+    handler = logging.FileHandler("ScriptLog_%s_%s_%s.log" % (now.day, now.month, now.year), "a", encoding=None, delay="true")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+    
+    # Output to screen
+    stdOutHandler = logging.StreamHandler()
+    stdOutHandler.setFormatter(formatter)
+    logger.addHandler(stdOutHandler)
 
     return logger
 
